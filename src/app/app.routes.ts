01b5d37f85084d0dd202fc9b580f0a0c1a59e611
hernanfloresramirez <hernan.flores@shipedge.com>
2025-04-27 02:12:08 -0400

    corrigiendo home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: 'homr', loadComponent: () => import('./core/pages/components/home/home.component') },
+    { path: 'home', loadComponent: () => import('./core/pages/components/home/home.component') },
     { path: '**', loadComponent: () => import('./core/pages/shared/error404/error404.component') }
 ];

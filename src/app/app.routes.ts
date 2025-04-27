cfec6d784dcefe9b85b6dbbd89dc2c7c4b8201ad
hernanfloresramirez <hernan.flores@shipedge.com>
2025-04-27 01:39:16 -0400

    Add pages home and error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'homr', loadComponent: () => import('./core/pages/components/home/home.component') },
+    { path: '**', loadComponent: () => import('./core/pages/shared/error404/error404.component') }
+];

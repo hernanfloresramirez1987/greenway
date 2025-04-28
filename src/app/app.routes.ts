48796a7f0b50804990aba11cbc82403a0c7e8e35
hernanfloresramirez <hernan.flores@shipedge.com>
2025-04-27 22:15:40 -0400

    Add contenido tailwindcsss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: 'home', loadComponent: () => import('./core/pages/components/home/home.component') },
-    { path: '**', loadComponent: () => import('./core/pages/shared/error404/error404.component') }
+    { path: 'home', loadComponent: () => import('./core/pages/home/home.component') },
+    { path: '**', loadComponent: () => import('./core/pages___/shared/error404/error404.component') }
 ];

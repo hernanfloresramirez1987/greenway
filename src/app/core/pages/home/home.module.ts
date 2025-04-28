0c9b397866b06490d2e2556be875a986238abeb6
hernanfloresramirez <hernan.flores@shipedge.com>
2025-04-27 23:54:21 -0400

    ADd funcion page home

--- src/app/core/pages/home/home.module.ts
+++ src/app/core/pages/home/home.module.ts
@@ -2,23 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomePageRoutingModule } from './home.routing.module';
 
-import { HomeComponent } from './home.component';
+// import { HomeComponent } from './home.component';
 
-import { LatestCatsModule } from 'src/app/components/latest-cats/latest-cats.module';
-import { LoaderModule } from 'src/app/components/loader/loader.module';
+// import { LatestCatsModule } from 'src/app/components/latest-cats/latest-cats.module';
+// import { LoaderModule } from 'src/app/components/loader/loader.module';
 @NgModule({
-  declarations: [
-    HomeComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     HomePageRoutingModule,
-    LatestCatsModule,
-    LoaderModule
+    // LatestCatsModule,
+    // LoaderModule
   ],
-  exports: [
-    HomeComponent
-  ]
+  exports: []
 })
 
 export class HomePageModule {}

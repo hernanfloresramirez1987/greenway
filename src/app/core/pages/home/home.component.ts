eba658a89f21f00f3c31c4c437ae1aa3b9cfab4c
hernanfloresramirez <hernan.flores@shipedge.com>
2025-04-27 22:24:07 -0400

    add mejoras del menu

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,9 +11,15 @@ export default class HomeComponent {
   suggestedCats: any = [];
   hidden: boolean = true;
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router) {
+    const carousel: any = document.getElementById('carousel');
+    let index = 0;
+
+    setInterval(() => {
+      index = (index + 1) % 3; // Número de slides
+      carousel.style.transform = `translateX(-${index * 100}%)`;
+    }, 4000); // Cambia cada 4 segundos
+  }
 
   mostSearched() {
     this.router.navigate(['/most-searched']);

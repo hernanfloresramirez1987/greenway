1904eb16249ef50ca77e868739e243ba21bfb8e9
Shipedge <hernan.flores@shipedge.com>
2025-05-25 16:27:32 -0400

    Add constaco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', loadComponent: () => import('./core/pages/home/home.component') },
     { path: 'services', loadComponent: () => import('./core/pages/services/services.component') },
-    { path: 'como-funciona', loadComponent: () => import('./core/pages/como-funciona/como-funciona.component') }
+    { path: 'como-funciona', loadComponent: () => import('./core/pages/como-funciona/como-funciona.component') },
+    { path: 'contactos', loadComponent: () => import('./core/pages/contactos/contactos.component') }
 ];
---
+++ src/app/core/pages/contactos/contactos.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+@Component({
+  selector: 'app-contactos',
+  imports: [GoogleMapsModule],
+  templateUrl: './contactos.component.html',
+  styleUrl: './contactos.component.css'
+})
+export default class ContactosComponent {
+
+  userLocation: google.maps.LatLngLiteral | null = null;
+  destination = { lat: -17.778136, lng: -63.196647 }; // Mercado Mutualista
+  zoom = 13;
+  center!: google.maps.LatLngLiteral;
+
+  ngOnInit(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.userLocation = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        };
+        this.center = this.userLocation;
+      });
+    }
+  }
+}

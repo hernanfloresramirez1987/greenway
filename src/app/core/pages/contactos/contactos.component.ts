71dd84a4a1593ddf5f9518036fd029bf556912c9
Shipedge <hernan.flores@shipedge.com>
2025-05-25 19:19:02 -0400

    Add mejoras de nombres

--- src/app/core/pages/contactos/contactos.component.ts
+++ src/app/core/pages/contactos/contactos.component.ts
@@ -1,38 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-contactos',
-  imports: [RouterModule, HeaderComponent, FooterComponent, GoogleMapsModule],
+  standalone: true,
+  imports: [RouterModule, HeaderComponent, FooterComponent, GoogleMapsModule, JsonPipe],
   templateUrl: './contactos.component.html',
   styleUrl: './contactos.component.css'
 })
 export default class ContactosComponent implements OnInit {
-
-  // userLocation: google.maps.LatLngLiteral | null = null;
-  // destination = { lat: -17.778136, lng: -63.196647 }; // Mercado Mutualista
-  // zoom = 13;
-  // center!: google.maps.LatLngLiteral;
-
-  // ngOnInit(): void {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(pos => {
-  //       this.userLocation = {
-  //         lat: pos.coords.latitude,
-  //         lng: pos.coords.longitude
-  //       };
-  //       this.center = this.userLocation;
-  //     });
-  //   }
-  // }
-
   userLocation: google.maps.LatLngLiteral | null = null;
   destination: google.maps.LatLngLiteral = { lat: -17.778136, lng: -63.196647 };
   center: google.maps.LatLngLiteral = this.destination;
   zoom = 13;
+
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     if (navigator.geolocation) {
@@ -42,13 +28,35 @@ export default class ContactosComponent implements OnInit {
           lng: pos.coords.longitude
         };
         this.center = this.userLocation;
+        this.cdr.detectChanges(); // <-- fuerza actualización
       }, err => {
         console.error('Error al obtener la ubicación:', err);
-        // fallback center
         this.center = this.destination;
       });
     } else {
       this.center = this.destination;
     }
   }
+  // userLocation: google.maps.LatLngLiteral | null = null;
+  // destination: google.maps.LatLngLiteral = { lat: -17.778136, lng: -63.196647 };
+  // center: google.maps.LatLngLiteral = this.destination;
+  // zoom = 13;
+
+  // ngOnInit(): void {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(pos => {
+  //       this.userLocation = {
+  //         lat: pos.coords.latitude,
+  //         lng: pos.coords.longitude
+  //       };
+  //       this.center = this.userLocation;
+  //     }, err => {
+  //       console.error('Error al obtener la ubicación:', err);
+  //       // fallback center
+  //       this.center = this.destination;
+  //     });
+  //   } else {
+  //     this.center = this.destination;
+  //   }
+  // }
 }

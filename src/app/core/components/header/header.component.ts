12dbf45a78498d7ca4c9253b68e5d81acf46097e
Shipedge <hernan.flores@shipedge.com>
2025-05-25 16:33:57 -0400

    Add despliegue del menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -8,5 +8,10 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log(this.isMenuOpen);
+  }
 }

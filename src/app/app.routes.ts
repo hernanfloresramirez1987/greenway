5dfd1d02cda8b229986be296d3501110751bc0dd
hernanfloresramirez <hernanfloresramirez1987@gmail.com>
2026-03-01 18:59:25 -0400

    add mejoas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: 'v1', pathMatch: 'full', redirectTo: 'home' },
     { path: 'v1/home', loadComponent: () => import('./core/pages/home/home.component') },
+    { path: 'v1/funcionalidades', loadComponent: () => import('./core/pages/funcionalidades/funcionalidades.component') }
     { path: 'v1/services', loadComponent: () => import('./core/pages/servicios/services.component') },
     { path: 'v1/como-funciona', loadComponent: () => import('./core/pages/como-funciona/como-funciona.component') },
     { path: 'v1/contactos', loadComponent: () => import('./core/pages/contactos/contactos.component') },
---
+++ src/app/core/pages/funcionalidades/funcionalidades.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-funcionalidades',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './funcionalidades.component.html',
+  styleUrl: './funcionalidades.component.css'
+})
+export default class FuncionalidadesComponent {
+
+}
